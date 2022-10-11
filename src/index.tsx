03318c79121d7c57353b2f84af486b1abeac439b
laststandjam <zaneshook@gmail.com>
2022-10-11 11:27:29 -0700

    readying lang routing

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+} from "react-router-dom";
 
+const router = createBrowserRouter([
+  {
+  },
+])
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
