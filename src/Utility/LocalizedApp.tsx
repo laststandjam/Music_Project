b6d5863fbd5afce164a095f7e20f35ae7c2388fa
laststandjam <zaneshook@gmail.com>
2022-10-25 08:59:24 -0700

    pre index change

---
+++ src/Utility/LocalizedApp.tsx
@@ -0,0 +1,36 @@
+import messages_en from "../../public/urls_en.json";
+import messages_sp from "../../public/urls_sp.json";
+
+import React, { useEffect, useMemo } from 'react';
+import { Navigate, useLocation, useNavigate } from 'react-router-dom';
+
+import App from '../App';
+import { IntlProvider } from 'react-intl';
+import Cookies from 'universal-cookie';
+
+export const messages: { [index: string]: any } = {
+    'en': messages_en,
+    'sk': messages_sp
+}
+
+interface LocalizedAppProps{
+    lang: string
+}
+
+export const LocalizedApp: React.FC<LocalizedAppProps> = ({lang}) => {
+    const cookies = useMemo(() => new Cookies(document.cookie), []);
+
+    useEffect(() => {
+        cookies.set('lang', lang, { path: '/' });
+    }, [lang, cookies])
+
+    return (
+        <IntlProvider
+            locale={lang}
+            messages={messages[lang]}
+            defaultLocale={"en"}
+        >
+            <App/>
+        </IntlProvider>
+    )
+}
