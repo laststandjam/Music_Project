ea80a51abd3e0d77a2048a6d6c08001b49915167
laststandjam <zaneshook@gmail.com>
2022-12-06 10:58:14 -0800

    tailwind cards

--- src/Components/Albums.tsx
+++ src/Components/Albums.tsx
@@ -1,71 +1,73 @@
-
-
+ 
+ 
 import {useState, useEffect}from 'react';
 import {Client} from "../Utility/Client"
 import { Album as AlbumType} from "../models/album"
 import {contentTypes} from "../project/contentTypes"
-
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Typography,
+} from "@material-tailwind/react";
+ 
 const Albums: React.FC = () =>{
-
-
+ 
+ 
         //State
     const [albums, setAlbums] = useState<AlbumType[]>([])
     const [loading, setLoading] = useState<boolean>(true)
-
-
+ 
+ 
 useEffect(() => {
-
+ 
     //call
     const fetchAlbums = async () =>{
        const req = await  Client.items<AlbumType>()
         .type(contentTypes.album.codename)
         .depthParameter(1)
-        
 /*future langauge stuff
      if(language){
         querys.languageParamter(languate)
      }*/
-
      .toPromise()
-    
-     
      setLoading(false)
-     
-     
      setAlbums(req.data.items)
-   
     }
-    
+
   fetchAlbums()
   console.log(albums[0])
    
 }, [loading])
-
+ 
 return(
-
-
+ 
+ 
 <>
-
-
-//loading component
-
+ 
 {loading
 ?<div>"Loading"</div>
 :
 <div className="display flex relative">
 {albums.map((album, i)=>(
-  <div className='shadow-md w-full relative bottom-50 left-50 flex' key={i}>
-  <h1>{album.elements.title.value}</h1>
-  <img src={album.elements.coverArt.linkedItems[0].elements.asset.value[0].url}/>
- 
-  </div>
+  <Card className='w96' key={i}>
+  <CardHeader>
+  <img className="" src={album.elements.coverArt.linkedItems[0].elements.asset.value[0].url}/>
+ </CardHeader>
+ <CardBody className="text-center">
+        <Typography variant="h5" className="mb-2">
+        {album.elements.title.value}
+        </Typography>
+  </CardBody>
+  </Card>
   ))}
 </div>}
 </>
 )
-
+ 
 }
-
-
-
+ 
+ 
+ 
 export {Albums}
